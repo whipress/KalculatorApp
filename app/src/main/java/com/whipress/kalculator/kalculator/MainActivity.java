e215abcefa144a2e5e51092e055385aafac0a43a
kasper <whipress@gmail.com>
2017-06-17 00:10:44 -0300

    REMOVE THE LOG ON THE CHECK 2ND OPERATION CONDITION

--- app/src/main/java/com/whipress/kalculator/kalculator/MainActivity.java
+++ app/src/main/java/com/whipress/kalculator/kalculator/MainActivity.java
@@ -2,7 +2,6 @@ package com.whipress.kalculator.kalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -128,7 +127,7 @@ int operationFinished = 0;
                     CalculationValues.setText("");
                 }
                 operationFinished = 0;
-                 Log.w("Operação 2?", "Esta é a segunda operação?");
+                
         }
 
 
